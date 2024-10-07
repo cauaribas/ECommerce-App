b136bc56012c61d1e306c1bc4d3bb7636eabe599
Cauã Ribas <100866872+cauaribas@users.noreply.github.com>
2024-10-07 20:30:15 -0300

    refactor: auth routes to use error handling middleware

--- src/routes/auth.ts
+++ src/routes/auth.ts
@@ -1,7 +1,8 @@
 import { FastifyInstance } from 'fastify';
 import { signup, login } from '../controllers/users/auth-controller';
+import { errorHandler } from '../error-handler';
 
 export async function authRoutes(app: FastifyInstance) {
-    app.post('/signup', signup);
-    app.post('/login', login);
+    app.post('/signup', errorHandler(signup));
+    app.post('/login', errorHandler(login));
 }
