2cd196778623533e7a187e4178b4249cf6f071de
Cauã Ribas <100866872+cauaribas@users.noreply.github.com>
2024-10-08 19:21:44 -0300

    feat: Add routes for product CRUD operations

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,11 @@
 import fastify from "fastify";
 import { authRoutes } from "./routes/auth";
 import { errorMiddleware } from "./middlewares/errors";
+import { productsRoutes } from "./routes/products";
 
 export const app = fastify();
 
 app.setErrorHandler(errorMiddleware);
 
 app.register(authRoutes, { prefix: '/auth' });
+app.register(productsRoutes);
