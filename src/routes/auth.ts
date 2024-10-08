4d0bd710f6d560c515ead122b863f799847554c5
Cauã Ribas <100866872+cauaribas@users.noreply.github.com>
2024-10-08 11:04:58 -0300

    refactor: Update auth routes to include "me" endpoint

--- src/routes/auth.ts
+++ src/routes/auth.ts
@@ -1,8 +1,10 @@
 import { FastifyInstance } from 'fastify';
-import { signup, login } from '../controllers/users/auth-controller';
+import { signup, login, me } from '../controllers/users/auth-controller';
 import { errorHandler } from '../error-handler';
+import { authMiddleware } from '../middlewares/auth';
 
 export async function authRoutes(app: FastifyInstance) {
     app.post('/signup', errorHandler(signup));
     app.post('/login', errorHandler(login));
+    app.get('/me', { onRequest: [ authMiddleware ] }, errorHandler(me));
 }
