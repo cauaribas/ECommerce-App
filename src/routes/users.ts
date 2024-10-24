9c63f162bcb6e8706d492390bcd283ec88076f1d
Cauã Ribas <100866872+cauaribas@users.noreply.github.com>
2024-10-24 18:21:55 -0300

    refactor: Update user address routes and import usersRoutes in app.ts

--- src/routes/users.ts
+++ src/routes/users.ts
@@ -1,12 +1,12 @@
 import { FastifyInstance } from "fastify";
 import { createAddress, deleteAddress, getAddresses, updateAddress } from "../controllers/users/user-addresses-controller";
 import { authMiddleware } from "../middlewares/auth";
-import { adminMiddleware } from "../middlewares/admin";
 import { errorHandler } from "../error-handler";
+import { updateUsers } from "../controllers/users/user";
 
 export async function usersRoutes(app: FastifyInstance) {
     app.post('/address', { onRequest: authMiddleware }, errorHandler(createAddress));
     app.get('/address', { onRequest: authMiddleware }, errorHandler(getAddresses));
-    app.patch('/address/:id', { onRequest: authMiddleware }, errorHandler(updateAddress));
     app.delete('/address/:id', { onRequest: authMiddleware }, errorHandler(deleteAddress));
+    app.put('/address', { onRequest: authMiddleware}, errorHandler(updateUsers));
 }
