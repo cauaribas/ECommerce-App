87efc4ea71843f99757999c572704f039e9e030a
Cauã Ribas <100866872+cauaribas@users.noreply.github.com>
2024-10-28 17:07:29 -0300

    refactor: Add ordersRoutes to app registration

--- src/app.ts
+++ src/app.ts
@@ -4,6 +4,7 @@ import { errorMiddleware } from "./middlewares/errors";
 import { productsRoutes } from "./routes/products";
 import { usersRoutes } from "./routes/users";
 import { cartRoutes } from "./routes/cart";
+import { ordersRoutes } from "./routes/orders";
 
 export const app = fastify();
 
@@ -13,3 +14,4 @@ app.register(authRoutes, { prefix: '/auth' });
 app.register(productsRoutes);
 app.register(usersRoutes, { prefix: '/users' });
 app.register(cartRoutes, { prefix: '/cart' });
+app.register(ordersRoutes, { prefix: '/orders' });
