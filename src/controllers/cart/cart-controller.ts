3061af101c85052b364a41401b079ab9adf0c085
Cauã Ribas <100866872+cauaribas@users.noreply.github.com>
2024-10-28 12:20:08 -0300

    refactor: Simplify deleteItemFromCart function in cart-controller.ts

--- src/controllers/cart/cart-controller.ts
+++ src/controllers/cart/cart-controller.ts
@@ -1,9 +1,12 @@
 import { FastifyReply, FastifyRequest } from "fastify";
-import { CreateCartSchema } from "../../schema/cart";
+import { CreateCartSchema, UpdateCartSchema } from "../../schema/cart";
 import { NotFoundException } from "../../exceptions/not-found";
 import { ErrorCode } from "../../exceptions/root";
 import { Product } from "@prisma/client";
 import { prisma } from "../../lib/prisma";
+import { ParamsSchema } from "../../schema/users";
+import { ConflictException } from "../../exceptions/conflict-exception";
+import { triggerAsyncId } from "async_hooks";
 
 export async function addItemToCart(request: FastifyRequest, reply: FastifyReply) {
     const { productId, quantity } = CreateCartSchema.parse(request.body);
@@ -19,10 +22,21 @@ export async function addItemToCart(request: FastifyRequest, reply: FastifyReply
             throw new NotFoundException("Product not found", null, ErrorCode.PRODUCT_NOT_FOUND);
         }
 
-        const cart = await prisma.cartItems.create({
+        const existingCartItem = await prisma.cartItem.findFirst({
+            where: {
+                userId: request.user.id,
+                productId,
+            },
+        });
+
+        if(existingCartItem) {
+            throw new ConflictException("Product already in cart", ErrorCode.PRODUCT_ALREADY_IN_CART);
+        }
+
+        const cart = await prisma.cartItem.create({
             data: {
                 userId: request.user.id,
-                productId: product.id,
+                productId,
                 quantity,
             }
         });
@@ -34,13 +48,57 @@ export async function addItemToCart(request: FastifyRequest, reply: FastifyReply
 }
 
 export async function deleteItemFromCart(request: FastifyRequest, reply: FastifyReply) {
+    const { id } = ParamsSchema.parse(request.params);
     
+    try {
+        await prisma.cartItem.delete({
+            where: {
+                userId: request.user.id,
+                id,
+            }
+        });
+
+        reply.send({success: true});
+    } catch (error) {
+        throw new NotFoundException("Failed to delete item from cart", error, ErrorCode.PRODUCT_NOT_FOUND);
+    }
 }
 
 export async function changeQuantity(request: FastifyRequest, reply: FastifyReply) {
+    const { id } = ParamsSchema.parse(request.params);
     
+    const { quantity } = UpdateCartSchema.parse(request.body);
+
+    try {
+        const updatedCart = await prisma.cartItem.update({
+            where: {
+                userId: request.user.id,
+                id,
+            },
+            data: {
+                quantity,
+            }
+        });
+
+        reply.send(updatedCart);
+    } catch (error) {
+        throw new NotFoundException("Failed to update cart item", error, ErrorCode.PRODUCT_NOT_FOUND);
+    }
 }
 
 export async function getCart(request: FastifyRequest, reply: FastifyReply) {
-    
+    try {
+        const cart = await prisma.cartItem.findMany({
+            where: {
+                userId: request.user.id
+            }, 
+            include: {
+                product: true
+            }
+        });
+
+        reply.send(cart);
+    } catch (error) {
+        throw new NotFoundException("Failed to get cart", error, ErrorCode.PRODUCT_NOT_FOUND);
+    }
 }
