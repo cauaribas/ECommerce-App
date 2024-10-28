d45b04a7bc727ccecd9cb8679e1226eb7572fb50
Cauã Ribas <100866872+cauaribas@users.noreply.github.com>
2024-10-28 17:08:33 -0300

    refactor: Update PrismaClient configuration to include formattedAddress in result

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -2,4 +2,21 @@ import { PrismaClient } from '@prisma/client';
 
 export const prisma = new PrismaClient({
     log: ["query"],
+}).$extends({
+    result: {
+        address: {
+            formattedAddress: {
+                needs: {
+                    lineOne: true,
+                    lineTwo: true,
+                    city: true, 
+                    country: true,
+                    pincode: true
+                },
+                compute: (address) => {
+                    return `${address.lineOne}, ${address.lineTwo}, ${address.city}, ${address.country}, ${address.pincode}`;
+                }
+            }
+        }
+    }
 });
