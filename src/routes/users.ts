0f0e72d1982c62dcba86f5f29377b91cf425e9e7
Cauã Ribas <100866872+cauaribas@users.noreply.github.com>
2024-10-29 16:50:18 -0300

    refactor: Update user routes and controllers

--- src/routes/users.ts
+++ src/routes/users.ts
@@ -2,11 +2,15 @@ import { FastifyInstance } from "fastify";
 import { createAddress, deleteAddress, getAddresses, updateAddress } from "../controllers/users/user-addresses-controller";
 import { authMiddleware } from "../middlewares/auth";
 import { errorHandler } from "../error-handler";
-import { updateUsers } from "../controllers/users/user";
+import { changeUserRole, getUserById, getUsers, updateUsers } from "../controllers/users/user";
+import { adminMiddleware } from "../middlewares/admin";
 
 export async function usersRoutes(app: FastifyInstance) {
     app.post('/address', { onRequest: authMiddleware }, errorHandler(createAddress));
     app.get('/address', { onRequest: authMiddleware }, errorHandler(getAddresses));
     app.delete('/address/:id', { onRequest: authMiddleware }, errorHandler(deleteAddress));
     app.put('/address', { onRequest: authMiddleware}, errorHandler(updateUsers));
+    app.put('/:id/role', { onRequest: [ authMiddleware, adminMiddleware] }, errorHandler(changeUserRole));
+    app.get('/', { onRequest: [ authMiddleware, adminMiddleware] }, errorHandler(getUsers));
+    app.get('/:id', { onRequest: [ authMiddleware, adminMiddleware] }, errorHandler(getUserById));
 }
