b107de4ac24cee5d7ee785d82b624a347326d0e8
Cauã Ribas <100866872+cauaribas@users.noreply.github.com>
2024-11-01 15:24:25 -0300

    refactor: Enhance orders routes with new endpoints for listing all orders and user-specific orders

--- src/routes/orders.ts
+++ src/routes/orders.ts
@@ -1,11 +1,15 @@
 import { FastifyInstance } from 'fastify';
 import { authMiddleware } from '../middlewares/auth';
-import { cancelOrder, createOrder, getOrderById, listOrders } from '../controllers/orders/orders';
+import { cancelOrder, changeStatus, createOrder, getOrderById, listAllOrders, listOrders, listUsersOrders } from '../controllers/orders/orders';
 import { errorHandler } from '../error-handler';
+import { adminMiddleware } from '../middlewares/admin';
 
 export async function ordersRoutes(app: FastifyInstance) {
     app.post('/', { onRequest: [ authMiddleware ] }, errorHandler(createOrder));
     app.get('/', { onRequest: [ authMiddleware ] }, errorHandler(listOrders));
-    app.put('/:id/cancel', { onRequest: [ authMiddleware ] }, errorHandler(cancelOrder));
     app.get('/:id', { onRequest: [ authMiddleware ] }, errorHandler(getOrderById));
+    app.get('/index', { onRequest: [ authMiddleware, adminMiddleware ] }, errorHandler(listAllOrders));
+    app.get('/users', { onRequest: [ authMiddleware ] }, errorHandler(listUsersOrders));
+    app.put('/:id/cancel', { onRequest: [ authMiddleware ] }, errorHandler(cancelOrder));
+    app.put('/:id/status', { onRequest: [ authMiddleware, adminMiddleware ] }, errorHandler(changeStatus));
 }
